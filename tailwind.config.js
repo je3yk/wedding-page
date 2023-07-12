22fea8e0107424c87a392c9ff3d7ebf8bf4f6ef8
Jędrzej Zawojski <jedrzej.zawojski95@gmail.com>
2023-07-12 13:50:13 +0200

    Sections template defined

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        "4xl": "1920px",
+      },
+      colors: {
+        // BLUE
+        blue: "#5073B8",
+        // NATURAL
+        "natural-90": "#000000",
+        "natural-50": "#999999",
+        natural: "#FFFFFF",
       },
     },
   },
   plugins: [],
-}
+};
