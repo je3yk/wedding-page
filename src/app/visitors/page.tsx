e0a75c04a6c87e85e8798c2aed5b61201180e06b
Jędrzej Zawojski <jedrzej.zawojski95@gmail.com>
2023-07-17 23:18:56 +0200

    Add visitors information page

---
+++ src/app/visitors/page.tsx
@@ -0,0 +1,12 @@
+import Section from "@/components/Section";
+import Typography from "@/components/Typography";
+
+export default function Visitors() {
+  return (
+    <Section id="visitors-main" className="min-h-screen pt-5">
+      <Typography variant="h1" className="text-blue text-center">
+        Informacje dla przyjezdnych
+      </Typography>
+    </Section>
+  );
+}
